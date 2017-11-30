5eee075ab1ffdcc1339dd0f043d5015f9b66e002
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-30 17:38:40 +0100

    live kanban

---
+++ Card.js
@@ -0,0 +1,53 @@
+// KLASA KANBAN CARD
+function Card(id, name) {
+	var self = this;
+	
+	this.id = id;
+	this.name = name || 'No name given';
+	this.element = createCard();
+
+	function createCard() {
+		var card = $('<li class="card"></li>');
+		var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+		var cardDescription = $('<p class="card-description"></p>');
+		
+		cardDeleteBtn.click(function(){
+			self.removeCard();
+		});
+		
+		card.append(cardDeleteBtn);
+		//cardDescription.text(self.description);
+		cardDescription.text(self.name);
+		card.append(cardDescription)
+		return card;
+	}
+}
+Card.prototype = {
+	/*  removeCard: function() {
+	  this.element.remove();
+	} */
+	removeCard: function() {
+    	var self = this;
+    	$.ajax({
+      		url: baseUrl + '/card/' + self.id,
+      		method: 'DELETE',
+      		success: function(){
+        		self.element.remove();
+      		}
+    	});
+	}
+}
+
+/*
+
+DELETE /card/{id}
+------------------------------
+Request:
+{id}: int - id card we want to remove
+------------------------------
+Response:
+{
+   id: int
+}
+
+*/
